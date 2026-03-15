f2d34eb456b5a41b0928fe2ef90cddd9fe539af3
Jorge Vergara Stuardo <jor.vergaras@duocuc.cl>
2026-03-15 17:47:44 -0300

    Microservicio de inventario con MYSQL

---
+++ src/main/java/com/shopSmart/inventario/model/Producto.java
@@ -0,0 +1,16 @@
+package com.shopSmart.inventario.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "productos")
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private double precio;
+    private Integer stock;
+}
